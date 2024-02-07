09eb7c664f963902b58923939c4dd058e8a2401c
BernardoSsilva <bernardo.silva0808@gmail.com>
2024-02-07 09:00:21 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .addBasicAuth()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  
+
   await app.listen(3001);
 }
 bootstrap();
